38cfcf09c680aa546d4d5eef56a0a72457850672
Samuel Morgan <samuel.edmund.morgan@icloud.com>
2025-09-05 12:49:17 +0300

    footer changes

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
+
 </main>
 
 <footer class="site-footer">
 	<div class="site-footer__inner">
-		<div class="site-footer__left">&copy; 2025 Національна академія СБУ</div>
+		<div class="site-footer__left">&copy; <?php echo date('Y'); ?> Національна академія СБУ</div>
 		<div class="site-footer__right">Підтримка: <a href="tel:+380445277690">527-76-90</a></div>
 	</div>
 </footer>
